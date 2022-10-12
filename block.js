14bfedf4c8ffa3178f942159c739cf621dae6d91
Bhargav Sarvaria <34936854+bhargav-sarvaria@users.noreply.github.com>
2022-10-11 17:09:02 -0700

    Flappy bird using p5

---
+++ block.js
@@ -0,0 +1,34 @@
+function Block() {
+    this.x = width;
+    this.y1 = 0;
+    this.h1 = Math.random() * height * 0.75;
+    this.y2 = this.h1 + height * 0.25;
+    this.h2 = height - this.y2;
+    this.width = width / 4;
+
+    this.display = function () {
+        fill(0, 255, 0);
+        rect(this.x, this.y1, this.width, this.h1);
+        rect(this.x, this.y2, this.width, this.h2);
+        line(this.x, this.h1 - (height / 15), this.x + this.width, this.h1 - (height / 15));
+        line(this.x, this.y2 + (height / 15), this.x + this.width, this.y2 + (height / 15));
+    }
+
+    this.update = function () {
+        this.x -= 2;
+    }
+
+    this.isDead = function () {
+        if (this.x < -this.width) {
+            return true;
+        } return false;
+    }
+
+    this.displayRed = function () {
+        fill(255, 0, 0);
+        rect(this.x, this.y1, this.width, this.h1);
+        rect(this.x, this.y2, this.width, this.h2);
+        line(this.x, this.h1 - (height / 15), this.x + this.width, this.h1 - (height / 15));
+        line(this.x, this.y2 + (height / 15), this.x + this.width, this.y2 + (height / 15));
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,91 @@
+enterActive = false;
+var score = 0;
+var count = 0;
+var num = 0;
+
+function setup() {
+    createCanvas(windowHeight * 0.5625, windowHeight);
+    background(50, 50, 255);
+    frameRate(60);
+    flappy = new Bird();
+    b = [];
+}
+
+function draw() {
+    background(125, 125, 255);
+
+    // displaying the bird
+    flappy.display();
+    flappy.update();
+
+    if (count++ % (int)(2.5 * height / 10) === 0) {
+        console.log("Block created");
+        b[num++] = new Block();
+    }
+
+    // displaying all active blocks
+    for (var i = 0; i < b.length; i++) {
+        if (b[i] != null) {
+            b[i].display();
+            b[i].update();
+
+            // checking for collisions
+            if (b[i].x < flappy.x && (b[i].x + flappy.x) > 0) {
+                if (flappy.y < b[i].h1 || flappy.y > b[i].y2) {
+                    console.log("DEATH");
+                    b[i].displayRed();
+                    stop();
+                }
+            }
+
+            // checking if the block is active
+            if (b[i].isDead()) {
+                b[i] = null;
+                console.log("Block destroyed");
+                score++;
+            }
+        }
+    }
+
+    // score
+    fill(0);
+    textSize(width / 10);
+    textAlign(CENTER);
+    text(score * 10, 7 * width / 8, 7.5 * height / 8);
+}
+
+function keyPressed() {
+    if (key === ' ') {
+        flappy.tapped();
+        console.log("Space Pressed");
+    }
+
+    if (keyCode === ENTER && enterActive) {
+        restart();
+    }
+}
+
+function mousePressed() {
+    flappy.tapped();
+    console.log("Mouse Pressed");
+}
+
+function stop() {
+    fill(0);
+    textSize(height / 18);
+    textAlign(CENTER);
+    text("SCORE " + score * 10, width / 2, height / 2);
+    text("\n\nPRESS ENTER", width / 2, height / 2);
+    enterActive = true;
+    noLoop();
+}
+
+function restart() {
+    b = [];
+    flappy.y = height / 2;
+    score = 0;
+    count = 0;
+    num = 0;
+    enterActive = false;
+    loop();
+}
